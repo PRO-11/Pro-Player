eeaa6d1fd6cd97d269338622ff60c809af2acfcd
PRATHAM GUPTA <91843874+PRO-11@users.noreply.github.com>
2023-05-11 12:17:10 +0530

    first commit

---
+++ server/api/auth.js
@@ -0,0 +1,27 @@
+import  express  from "express";
+import { googleAuth, signin, signup} from "./controllers/auth.js";
+import User from "../models/User.js"
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+const router =express.Router();
+
+//Signup
+router.post("/signup",async(req,res)=>{
+    const salt=bcrypt.genSaltSync(10)
+    const hash=bcrypt.hashSync(req.body.password,salt)
+    console.log(req.body)
+    const newUser=new User({...req.body,password:hash})
+
+    await newUser.save()
+    res.status(200).json({msg:"User has been created",hash })
+})
+
+//Signin
+router.post("/signin",signin)
+
+//Google Auth
+router.post("/google",googleAuth)
+
+
+
+export default router
---
+++ server/index.js
@@ -0,0 +1,65 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import userRoutes from "./api/user.js"
+import authRoutes from "./api/auth.js"
+import videoRoutes from "./api/video.js"
+import commentRoutes from "./api/comment.js"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import path from "path"
+import { fileURLToPath } from 'url';
+
+dotenv.config()
+const app=express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const connect=()=>{
+  mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to db")
+  }).catch((err)=>{
+    throw err
+  })
+}
+app.use(cors());
+app.use(cookieParser())
+app.use(express.json())
+
+
+app.use("/api/auth",authRoutes) 
+app.use("/api/users",userRoutes)
+app.use("/api/videos",videoRoutes)
+app.use("/api/comments",commentRoutes)
+
+
+//error handling middleware
+app.use((err,req,res,next)=>{
+    const status=err.status || 500
+    const message=err.message || "Something went wrong!"
+
+    return res.status(status).json({
+        success:false,
+        status,
+        message
+    })
+})
+
+console.log(path.resolve(__dirname,'build'))
+
+
+
+if(process.env.NODE_ENV=='production'){
+  
+  console.log("Serve file")
+  app.get('/',(req,res)=>{
+      app.use(express.static(path.resolve(__dirname,'frontend','build')))
+      res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+  })
+}
+
+const PORT=process.env.PORT||5000
+app.listen(PORT,()=>{
+    connect()
+    console.log("Server is running on port 8081")
+})
