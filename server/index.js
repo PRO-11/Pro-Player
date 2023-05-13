d9ccd03d425b5d98acdc5d6c2f7d6a24c6a04054
PRATHAM GUPTA <91843874+PRO-11@users.noreply.github.com>
2023-05-13 16:05:08 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ if(process.env.NODE_ENV=='production'){
   console.log("Serve file")
   app.get('/',(req,res)=>{
     app.use(express.static(path.resolve(__dirname,'frrontend','build')))
-    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+    res.sendFile(path.resolve(__dirname,'frrontend','build','index.html'))
 })
 }
 
