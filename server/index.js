cf2097358d6bfd25e182aacecafc73dae5ce44b7
PRATHAM GUPTA <91843874+PRO-11@users.noreply.github.com>
2023-05-13 16:02:39 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -53,10 +53,9 @@ if(process.env.NODE_ENV=='production'){
   
   console.log("Serve file")
   app.get('/',(req,res)=>{
-
-      // app.use(express.static(path.resolve(__dirname,'frontend','build')))
-      res.send("Not Found")
-  })
+    app.use(express.static(path.resolve(__dirname,'frontend','build')))
+    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+})
 }
 
 const PORT=process.env.PORT||5000
